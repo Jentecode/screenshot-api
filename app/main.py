984af5924ae34f16a093d0ff632eb35bd8cb9be4
Jente Peeters <205074723+Jentecode@users.noreply.github.com>
2026-06-22 17:36:00 +0200

    main gemaakt

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-key = f"{url}_{width}_{height}"
+from app.auth import check_API 
+from app.cache import get_screenshot, save_screenshot
+from app.capture import take_screenshot
+from fastapi import FastAPI, Response, Depends
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/screenshots")
+async def screenshot_response(url: str, width: int, height: int, commons: Annotated[dict, Depends(check_API)]):
+
+    key = f"{url}_{width}_{height}"
+
+    key_controle = get_screenshot(key)
+
+    if key_controle is None:
+        screen_bytes = await take_screenshot(url, width, height)
+        save_screenshot(key, screen_bytes)
+        return Response(content=screen_bytes, media_type="image/png")    
+    else:
+        return Response(content=key_controle, media_type="image/png")    
+
